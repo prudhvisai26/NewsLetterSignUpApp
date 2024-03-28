b01641eb2d660d600d74d0ace4af109d5d30e27c
Prudhvi21 <dasariprudhvisai26@gmail.com>
2024-03-28 18:11:22 +0530

    Newslettter App

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fname;
+  const latsName = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: latsName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us18.api.mailchimp.com/3.0/lists/ea34834566";
+
+  const options = {
+    method: "POST",
+    auth: "luffy:654b69e3eaceb783984dc4ad94813d79-us18",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port:3000");
+});
+
+//API Key
+//654b69e3eaceb783984dc4ad94813d79-us18
+
+//List Id
+//ea34834566
